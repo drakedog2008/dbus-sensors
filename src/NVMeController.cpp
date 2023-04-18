5876fc43d78c6210c4e56bc1c87344cf4ab76a9f
Jinliang Wang <jinliangw@google.com>
2023-04-18 16:24:38 -0700

    nvmesensor: close pipe file descriptor before exception
    Close pipe file descriptors before throwing exception to prevent
    the potential leak of file desctriptor.
    
    Change-Id: Iffcb0e043307ea5774c9b51743ff8accf3400c82
    Signed-off-by: Jinliang Wang <jinliangw@google.com>

--- src/NVMeController.cpp
+++ src/NVMeController.cpp
@@ -193,12 +193,16 @@ sdbusplus::message::unix_fd NVMeControllerEnabled::getLogPage(uint8_t lid,
         }
         else // No VU LogPage handler
         {
+            ::close(pipe[0]);
+            ::close(pipe[1]);
             throw sdbusplus::xyz::openbmc_project::Common::Error::
                 InvalidArgument();
         }
     }
     else // No VU plugin
     {
+        ::close(pipe[0]);
+        ::close(pipe[1]);
         throw sdbusplus::xyz::openbmc_project::Common::Error::InvalidArgument();
     }
     return sdbusplus::message::unix_fd{pipe[0]};
